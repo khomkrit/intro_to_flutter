27514fb9960a3379c49f14f7d4b5f045f40d523b
Khomkrit Sriwichai <khomkrit.sri@gmail.com>
2020-10-16 14:42:24 +0700

    Demo Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Hello World', textDirection: TextDirection.ltr)
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello Flutter')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+              child: Center(
+                child: Text('Item 1'),
+              ),
+            ),
+            Text('Item 2'),
+            Text('Item 3'),
+            Text('Item 4'),
+            Text('Item 5'),
+          ],
+        ),
+      ),
     );
   }
 }
